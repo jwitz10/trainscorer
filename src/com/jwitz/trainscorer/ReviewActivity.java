874c38e3f5cdd5f4d9b19824e7c48ecb969dcbd1
Justin Witz <witz.justin@gmail.com>
2014-01-25 19:10:57 -0600

    Show train length summary

--- src/com/jwitz/trainscorer/ReviewActivity.java
+++ src/com/jwitz/trainscorer/ReviewActivity.java
@@ -15,6 +15,16 @@ public class ReviewActivity extends Activity {;
     TextView trainReview;                
     TextView ticketReview;             
     TextView bonusReview;
+    TextView train1Review;
+    TextView train2Review;
+    TextView train3Review;
+    TextView train4Review;
+    TextView train5Review;
+    TextView train6Review;
+    TextView train7Review;
+    TextView train8Review;
+    TextView train9Review;
+    
     boolean redFilledIn;
     boolean greenFilledIn;
     boolean blueFilledIn;
@@ -37,6 +47,15 @@ public class ReviewActivity extends Activity {;
                 trainReview = (TextView) findViewById(R.id.redTrainReview);                
                 ticketReview = (TextView) findViewById(R.id.redTicketReview);                
                 bonusReview = (TextView) findViewById(R.id.redBonusReview);
+                train1Review = (TextView) findViewById(R.id.redTrain1Review);
+                train2Review = (TextView) findViewById(R.id.redTrain2Review);
+                train3Review = (TextView) findViewById(R.id.redTrain3Review);
+                train4Review = (TextView) findViewById(R.id.redTrain4Review);
+                train5Review = (TextView) findViewById(R.id.redTrain5Review);
+                train6Review = (TextView) findViewById(R.id.redTrain6Review);
+                train7Review = (TextView) findViewById(R.id.redTrain7Review);
+                train8Review = (TextView) findViewById(R.id.redTrain8Review);
+                train9Review = (TextView) findViewById(R.id.redTrain9Review);
                 redFilledIn = true;
                 
             }else if(scoreSummary.getPlayerNumber() == R.id.secondPlayerButton) {
@@ -45,6 +64,15 @@ public class ReviewActivity extends Activity {;
                 trainReview = (TextView) findViewById(R.id.greenTrainReview);                
                 ticketReview = (TextView) findViewById(R.id.greenTicketReview);                
                 bonusReview = (TextView) findViewById(R.id.greenBonusReview);
+                train1Review = (TextView) findViewById(R.id.greenTrain1Review);
+                train2Review = (TextView) findViewById(R.id.greenTrain2Review);
+                train3Review = (TextView) findViewById(R.id.greenTrain3Review);
+                train4Review = (TextView) findViewById(R.id.greenTrain4Review);
+                train5Review = (TextView) findViewById(R.id.greenTrain5Review);
+                train6Review = (TextView) findViewById(R.id.greenTrain6Review);
+                train7Review = (TextView) findViewById(R.id.greenTrain7Review);
+                train8Review = (TextView) findViewById(R.id.greenTrain8Review);
+                train9Review = (TextView) findViewById(R.id.greenTrain9Review);
                 greenFilledIn = true;
                 
             }else if(scoreSummary.getPlayerNumber() == R.id.thirdPlayerButton) {
@@ -53,6 +81,15 @@ public class ReviewActivity extends Activity {;
                 trainReview = (TextView) findViewById(R.id.blueTrainReview);                
                 ticketReview = (TextView) findViewById(R.id.blueTicketReview);                
                 bonusReview = (TextView) findViewById(R.id.blueBonusReview);
+                train1Review = (TextView) findViewById(R.id.blueTrain1Review);
+                train2Review = (TextView) findViewById(R.id.blueTrain2Review);
+                train3Review = (TextView) findViewById(R.id.blueTrain3Review);
+                train4Review = (TextView) findViewById(R.id.blueTrain4Review);
+                train5Review = (TextView) findViewById(R.id.blueTrain5Review);
+                train6Review = (TextView) findViewById(R.id.blueTrain6Review);
+                train7Review = (TextView) findViewById(R.id.blueTrain7Review);
+                train8Review = (TextView) findViewById(R.id.blueTrain8Review);
+                train9Review = (TextView) findViewById(R.id.blueTrain9Review);
                 blueFilledIn = true;
                 
             }else if(scoreSummary.getPlayerNumber() == R.id.fourthPlayerButton) {
@@ -61,6 +98,15 @@ public class ReviewActivity extends Activity {;
                 trainReview = (TextView) findViewById(R.id.yellowTrainReview);                
                 ticketReview = (TextView) findViewById(R.id.yellowTicketReview);                
                 bonusReview = (TextView) findViewById(R.id.yellowBonusReview);
+                train1Review = (TextView) findViewById(R.id.yellowTrain1Review);
+                train2Review = (TextView) findViewById(R.id.yellowTrain2Review);
+                train3Review = (TextView) findViewById(R.id.yellowTrain3Review);
+                train4Review = (TextView) findViewById(R.id.yellowTrain4Review);
+                train5Review = (TextView) findViewById(R.id.yellowTrain5Review);
+                train6Review = (TextView) findViewById(R.id.yellowTrain6Review);
+                train7Review = (TextView) findViewById(R.id.yellowTrain7Review);
+                train8Review = (TextView) findViewById(R.id.yellowTrain8Review);
+                train9Review = (TextView) findViewById(R.id.yellowTrain9Review);
                 yellowFilledIn = true;
                 
             }else if(scoreSummary.getPlayerNumber() == R.id.fifthPlayerButton) {
@@ -69,6 +115,15 @@ public class ReviewActivity extends Activity {;
                 trainReview = (TextView) findViewById(R.id.purpleTrainReview);                
                 ticketReview = (TextView) findViewById(R.id.purpleTicketReview);                
                 bonusReview = (TextView) findViewById(R.id.purpleBonusReview);
+                train1Review = (TextView) findViewById(R.id.purpleTrain1Review);
+                train2Review = (TextView) findViewById(R.id.purpleTrain2Review);
+                train3Review = (TextView) findViewById(R.id.purpleTrain3Review);
+                train4Review = (TextView) findViewById(R.id.purpleTrain4Review);
+                train5Review = (TextView) findViewById(R.id.purpleTrain5Review);
+                train6Review = (TextView) findViewById(R.id.purpleTrain6Review);
+                train7Review = (TextView) findViewById(R.id.purpleTrain7Review);
+                train8Review = (TextView) findViewById(R.id.purpleTrain8Review);
+                train9Review = (TextView) findViewById(R.id.purpleTrain9Review);
                 purpleFilledIn = true;
                 
             }else if(scoreSummary.getPlayerNumber() == R.id.sixthPlayerButton) {
@@ -77,6 +132,15 @@ public class ReviewActivity extends Activity {;
                 trainReview = (TextView) findViewById(R.id.blackTrainReview);                
                 ticketReview = (TextView) findViewById(R.id.blackTicketReview);                
                 bonusReview = (TextView) findViewById(R.id.blackBonusReview);
+                train1Review = (TextView) findViewById(R.id.blackTrain1Review);
+                train2Review = (TextView) findViewById(R.id.blackTrain2Review);
+                train3Review = (TextView) findViewById(R.id.blackTrain3Review);
+                train4Review = (TextView) findViewById(R.id.blackTrain4Review);
+                train5Review = (TextView) findViewById(R.id.blackTrain5Review);
+                train6Review = (TextView) findViewById(R.id.blackTrain6Review);
+                train7Review = (TextView) findViewById(R.id.blackTrain7Review);
+                train8Review = (TextView) findViewById(R.id.blackTrain8Review);
+                train9Review = (TextView) findViewById(R.id.blackTrain9Review);
                 blackFilledIn = true;
             }
             
@@ -97,7 +161,16 @@ public class ReviewActivity extends Activity {;
         overallReview.setText(String.valueOf(scoreSummary.getTotalScore()));
         trainReview.setText(String.valueOf(scoreSummary.getTrainScore()));
         ticketReview.setText(String.valueOf(scoreSummary.getTicketScore()));
-        bonusReview.setText(String.valueOf(scoreSummary.getBonusScore()));        
+        bonusReview.setText(String.valueOf(scoreSummary.getBonusScore()));
+        train1Review.setText(String.valueOf(scoreSummary.getTrains(1)));
+        train2Review.setText(String.valueOf(scoreSummary.getTrains(2)));
+        train3Review.setText(String.valueOf(scoreSummary.getTrains(3)));
+        train4Review.setText(String.valueOf(scoreSummary.getTrains(4)));
+        train5Review.setText(String.valueOf(scoreSummary.getTrains(5)));
+        train6Review.setText(String.valueOf(scoreSummary.getTrains(6)));
+        train7Review.setText(String.valueOf(scoreSummary.getTrains(7)));
+        train8Review.setText(String.valueOf(scoreSummary.getTrains(8)));
+        train9Review.setText(String.valueOf(scoreSummary.getTrains(9)));
     }
     
     public void hideSummaryWithNoScores() {
@@ -105,13 +178,32 @@ public class ReviewActivity extends Activity {;
         TextView trainReview;                
         TextView ticketReview;             
         TextView bonusReview;
+        TextView train1Review;
+        TextView train2Review;
+        TextView train3Review;
+        TextView train4Review;
+        TextView train5Review;
+        TextView train6Review;
+        TextView train7Review;
+        TextView train8Review;
+        TextView train9Review;
         
         if(!redFilledIn) {
             overallReview = (TextView) findViewById(R.id.redReview);                
             trainReview = (TextView) findViewById(R.id.redTrainReview);                
             ticketReview = (TextView) findViewById(R.id.redTicketReview);                
             bonusReview = (TextView) findViewById(R.id.redBonusReview);
-            hideViews(new TextView[]{overallReview, trainReview, ticketReview, bonusReview});
+            train1Review = (TextView) findViewById(R.id.redTrain1Review);
+            train2Review = (TextView) findViewById(R.id.redTrain2Review);
+            train3Review = (TextView) findViewById(R.id.redTrain3Review);
+            train4Review = (TextView) findViewById(R.id.redTrain4Review);
+            train5Review = (TextView) findViewById(R.id.redTrain5Review);
+            train6Review = (TextView) findViewById(R.id.redTrain6Review);
+            train7Review = (TextView) findViewById(R.id.redTrain7Review);
+            train8Review = (TextView) findViewById(R.id.redTrain8Review);
+            train9Review = (TextView) findViewById(R.id.redTrain9Review);
+            hideViews(new TextView[]{overallReview, trainReview, ticketReview, bonusReview,
+                    train1Review, train2Review, train3Review, train4Review, train5Review, train6Review, train7Review, train8Review, train9Review});
         }
         
         if(!greenFilledIn) {
@@ -119,7 +211,17 @@ public class ReviewActivity extends Activity {;
             trainReview = (TextView) findViewById(R.id.greenTrainReview);                
             ticketReview = (TextView) findViewById(R.id.greenTicketReview);                
             bonusReview = (TextView) findViewById(R.id.greenBonusReview);
-            hideViews(new TextView[]{overallReview, trainReview, ticketReview, bonusReview});
+            train1Review = (TextView) findViewById(R.id.greenTrain1Review);
+            train2Review = (TextView) findViewById(R.id.greenTrain2Review);
+            train3Review = (TextView) findViewById(R.id.greenTrain3Review);
+            train4Review = (TextView) findViewById(R.id.greenTrain4Review);
+            train5Review = (TextView) findViewById(R.id.greenTrain5Review);
+            train6Review = (TextView) findViewById(R.id.greenTrain6Review);
+            train7Review = (TextView) findViewById(R.id.greenTrain7Review);
+            train8Review = (TextView) findViewById(R.id.greenTrain8Review);
+            train9Review = (TextView) findViewById(R.id.greenTrain9Review);
+            hideViews(new TextView[]{overallReview, trainReview, ticketReview, bonusReview,
+                    train1Review, train2Review, train3Review, train4Review, train5Review, train6Review, train7Review, train8Review, train9Review});
         }
         
         if(!blueFilledIn) {
@@ -127,7 +229,17 @@ public class ReviewActivity extends Activity {;
             trainReview = (TextView) findViewById(R.id.blueTrainReview);                
             ticketReview = (TextView) findViewById(R.id.blueTicketReview);                
             bonusReview = (TextView) findViewById(R.id.blueBonusReview);
-            hideViews(new TextView[]{overallReview, trainReview, ticketReview, bonusReview});
+            train1Review = (TextView) findViewById(R.id.blueTrain1Review);
+            train2Review = (TextView) findViewById(R.id.blueTrain2Review);
+            train3Review = (TextView) findViewById(R.id.blueTrain3Review);
+            train4Review = (TextView) findViewById(R.id.blueTrain4Review);
+            train5Review = (TextView) findViewById(R.id.blueTrain5Review);
+            train6Review = (TextView) findViewById(R.id.blueTrain6Review);
+            train7Review = (TextView) findViewById(R.id.blueTrain7Review);
+            train8Review = (TextView) findViewById(R.id.blueTrain8Review);
+            train9Review = (TextView) findViewById(R.id.blueTrain9Review);
+            hideViews(new TextView[]{overallReview, trainReview, ticketReview, bonusReview,
+                    train1Review, train2Review, train3Review, train4Review, train5Review, train6Review, train7Review, train8Review, train9Review});
         }
         
         if(!yellowFilledIn) {
@@ -135,7 +247,17 @@ public class ReviewActivity extends Activity {;
             trainReview = (TextView) findViewById(R.id.yellowTrainReview);                
             ticketReview = (TextView) findViewById(R.id.yellowTicketReview);                
             bonusReview = (TextView) findViewById(R.id.yellowBonusReview);
-            hideViews(new TextView[]{overallReview, trainReview, ticketReview, bonusReview});
+            train1Review = (TextView) findViewById(R.id.yellowTrain1Review);
+            train2Review = (TextView) findViewById(R.id.yellowTrain2Review);
+            train3Review = (TextView) findViewById(R.id.yellowTrain3Review);
+            train4Review = (TextView) findViewById(R.id.yellowTrain4Review);
+            train5Review = (TextView) findViewById(R.id.yellowTrain5Review);
+            train6Review = (TextView) findViewById(R.id.yellowTrain6Review);
+            train7Review = (TextView) findViewById(R.id.yellowTrain7Review);
+            train8Review = (TextView) findViewById(R.id.yellowTrain8Review);
+            train9Review = (TextView) findViewById(R.id.yellowTrain9Review);
+            hideViews(new TextView[]{overallReview, trainReview, ticketReview, bonusReview,
+                    train1Review, train2Review, train3Review, train4Review, train5Review, train6Review, train7Review, train8Review, train9Review});
         }
         
         if(!purpleFilledIn) {
@@ -143,15 +265,35 @@ public class ReviewActivity extends Activity {;
             trainReview = (TextView) findViewById(R.id.purpleTrainReview);                
             ticketReview = (TextView) findViewById(R.id.purpleTicketReview);                
             bonusReview = (TextView) findViewById(R.id.purpleBonusReview);
-            hideViews(new TextView[]{overallReview, trainReview, ticketReview, bonusReview});
+            train1Review = (TextView) findViewById(R.id.purpleTrain1Review);
+            train2Review = (TextView) findViewById(R.id.purpleTrain2Review);
+            train3Review = (TextView) findViewById(R.id.purpleTrain3Review);
+            train4Review = (TextView) findViewById(R.id.purpleTrain4Review);
+            train5Review = (TextView) findViewById(R.id.purpleTrain5Review);
+            train6Review = (TextView) findViewById(R.id.purpleTrain6Review);
+            train7Review = (TextView) findViewById(R.id.purpleTrain7Review);
+            train8Review = (TextView) findViewById(R.id.purpleTrain8Review);
+            train9Review = (TextView) findViewById(R.id.purpleTrain9Review);
+            hideViews(new TextView[]{overallReview, trainReview, ticketReview, bonusReview,
+                    train1Review, train2Review, train3Review, train4Review, train5Review, train6Review, train7Review, train8Review, train9Review});
         }
         
         if(!blackFilledIn) {
-            overallReview = (TextView) findViewById(R.id.blackReview);                
+            overallReview = (TextView) findViewById(R.id.blackReview);
             trainReview = (TextView) findViewById(R.id.blackTrainReview);                
             ticketReview = (TextView) findViewById(R.id.blackTicketReview);                
             bonusReview = (TextView) findViewById(R.id.blackBonusReview);
-            hideViews(new TextView[]{overallReview, trainReview, ticketReview, bonusReview});
+            train1Review = (TextView) findViewById(R.id.blackTrain1Review);
+            train2Review = (TextView) findViewById(R.id.blackTrain2Review);
+            train3Review = (TextView) findViewById(R.id.blackTrain3Review);
+            train4Review = (TextView) findViewById(R.id.blackTrain4Review);
+            train5Review = (TextView) findViewById(R.id.blackTrain5Review);
+            train6Review = (TextView) findViewById(R.id.blackTrain6Review);
+            train7Review = (TextView) findViewById(R.id.blackTrain7Review);
+            train8Review = (TextView) findViewById(R.id.blackTrain8Review);
+            train9Review = (TextView) findViewById(R.id.blackTrain9Review);
+            hideViews(new TextView[]{overallReview, trainReview, ticketReview, bonusReview,
+                    train1Review, train2Review, train3Review, train4Review, train5Review, train6Review, train7Review, train8Review, train9Review});
         }
     }
     
